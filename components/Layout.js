033230fb05a3b1c7fdcb3ec4f1c44d80236e8d57
seif allah ben ahmed <benahmed.seifallah@gmail.com>
2021-07-14 21:43:21 +0200

    NextJS front-end DJ Events App

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import styles from "@/styles/Layout.module.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import ShowCase from "./ShowCase";
+
+export default function Layout({ title, keywords, description, children }) {
+    const router = useRouter();
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name=" description" content={description} />
+                <meta name=" keywords" content={keywords} />
+            </Head>
+            <Header />
+            {router.pathname === "/" && <ShowCase />}
+            <div className={styles.container}>{children}</div>
+            <Footer />
+        </div>
+    );
+}
+Layout.defaultProps = {
+    title: "DJ Events | find the hottest parties",
+    description: "find the latest DJ and other musical events",
+    keywords: "music ,dj,edm,events",
+};
---
+++ pages/events/[slug].js
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import { useRouter } from "next/router";
+
+export default function EventPage({ event }) {
+    const router = useRouter();
+
+    return (
+        <Layout>
+            <div className={styles.event}>
+                {/* <div className={styles.controls}>
+                    <Link href={`/events/edit/${event.id}`}>
+                        <a>
+                            <FaPencilAlt /> Edit Event
+                        </a>
+                    </Link>
+                    <a className={styles.delete} onClick={deleteEvent}>
+                        <FaTimes /> Delete Event
+                    </a>
+                </div> */}
+                <span>
+                    {new Date(event.date).toLocaleDateString("en-US")} at{" "}
+                    {event.time}
+                </span>
+                <h1>{event.name}</h1>
+                <ToastContainer />
+                {event.image && (
+                    <div className={styles.image}>
+                        <Image
+                            src={event.image.formats.medium.url}
+                            width={960}
+                            height={600}
+                        />
+                    </div>
+                )}
+                <h3>Performers :</h3>
+                <p>{event.performers}</p>
+                <h3>Description:</h3>
+                <p> {event.description} </p>
+                <h3> Venue:</h3>
+                <p> {event.venue} </p>
+                <p> {event.address} </p>
+                <Link href="/events">
+                    <a className={styles.back}>{"<"} Go Back</a>
+                </Link>
+            </div>
+        </Layout>
+    );
+}
+// export async function getServerSideProps({ query: { slug } }) {
+//     const res = await fetch(`${API_URL}/api/events/${slug}`);
+//     const event = await res.json();
+//     return {
+//         props: { event: event[0] },
+//     };
+// }
+
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/events`);
+    const events = await res.json();
+    const paths = events.map((event) => ({
+        params: { slug: event.slug },
+    }));
+    return {
+        paths,
+        fallback: true, //false : if notFound redirect to 404page
+    };
+}
+export async function getStaticProps({ params: { slug } }) {
+    const res = await fetch(`${API_URL}/events?slug=${slug}`);
+    const event = await res.json();
+
+    return {
+        props: { event: event[0] },
+        revalidate: 1,
+    };
+}
