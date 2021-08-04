6a43603982bbcf69c4a7f8471edf12879a672ca8
seif allah ben ahmed <benahmed.seifallah@gmail.com>
2021-08-04 23:50:29 +0200

    fix cors

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL; //Strapi API
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL; //Next API
+export const API_URL =
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"; //Strapi API
+export const NEXT_URL =
+    process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000"; //Next API
 export const PER_PAGE = 4;
